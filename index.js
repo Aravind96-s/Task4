050ea61e7c4b91a52f0b7d581c457e08c595f0ab
Aravind96-s <aravindrudra1996@gmail.com>
2023-11-03 23:16:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+fetch('https://jsonplaceholder.typicode.com/users') .then((data)=>{
+return data.json();
+}).then((objectData)=>{
+    console.log(objectData[0].name);
+    let tableData="";
+    objectData.map((values)=>{
+        tableData+=`<div class="card">
+        <p>Name:${values.name}</p>
+        <p>Mail:${values.email}</p>
+        <p>Street:${values.address.street}</p>
+        <p>City:${values.address.city}</p>
+    </div>`
+    });
+    document.getElementById("tcontent").innerHTML=tableData;
+})
+//watch video https://www.youtube.com/watch?v=vvjaRIM4Bjs
+   
