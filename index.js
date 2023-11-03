b2c3e16528755073cd9829a1bd99457c44bae024
Aravind Somasundaram <62368290+Aravind96-s@users.noreply.github.com>
2023-11-03 23:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ return data.json();
     let tableData="";
     objectData.map((values)=>{
         tableData+=`<div class="card">
-        <p>Name:${values.name}</p>
-        <p>Mail:${values.email}</p>
-        <p>Street:${values.address.street}</p>
-        <p>City:${values.address.city}</p>
+        <p>Name : ${values.name}</p>
+        <p>Mail : ${values.email}</p>
+        <p>Street : ${values.address.street}</p>
+        <p>City : ${values.address.city}</p>
     </div>`
     });
     document.getElementById("tcontent").innerHTML=tableData;
